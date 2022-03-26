38fba154fabec2f745cd92c5ff84c0e8982ee927
Justine <justinelouis.quijano@ama.edu.ph>
2022-03-26 17:20:21 +0800

    Created folder and route for tests.

---
+++ src/pages/login_copy.jsx
@@ -0,0 +1,212 @@
+import React, { Component, Fragment, useState } from 'react';
+import { Helmet } from "react-helmet-async";
+import { Badge, Box, Button, Center, Divider, FormControl, FormLabel, Image, Input, Text, VStack, HStack, Stack } from "@chakra-ui/react";
+import ecs_logo from '../assets/images/ECS-Logo-300dpi.png';
+import { PulseLoader } from "react-spinners";
+import { useNavigate } from 'react-router-dom';
+import { PageBaseUrl } from '../utils/urlbase';
+import { Maps, Pin } from '../components/maps';
+import { useAuth } from '../libs/auth';
+import {Previewer} from '../components/pdf';
+import FsedOneOfFour from '../components/fsed-forms/FsedOneOfFour';
+import Sampleform from '../components/Sampleform';
+import FsedTwoOfFour from '../components/fsed-forms/FsedTwoOfFour';
+import { useFsed } from '../libs/fsed';
+import FsedThreeOfFour from '../components/fsed-forms/FsedThreeOfFour';
+import {  Document} from '@react-pdf/renderer';
+import FsedFourOfFour from '../components/fsed-forms/FsedFourOfFour';
+
+
+
+export default function Login() {
+    const navigate = useNavigate();
+    const [ loadingState, setLoadingState ] = useState(false);
+    const { Login } = useAuth()
+    const {fsedValue} = useFsed()
+    const [pdfValue,setPdfValue]= useState({})
+
+    const authFromBackend = () => {
+       setLoadingState(true)
+       Login({userId:"test",password:"tests"})
+       
+    }
+
+
+    console.log(fsedValue)
+
+    return (
+        <React.Fragment>
+            <div>
+                <Helmet title={'Login'}></Helmet>
+            </div>
+            <Center  
+               bg={'gray.50'} 
+               height={'100vh'} 
+               display="flex"
+               margin={"30px"}
+            >
+                <Sampleform  />
+                     <Previewer pdfTitle={"FSED FORM"}>
+                         
+                         <Document>
+                             <FsedOneOfFour pdfValue={fsedValue}/>
+                             <FsedTwoOfFour pdfValue={fsedValue}/>
+                             <FsedThreeOfFour pdfValue={fsedValue}/>
+                             <FsedFourOfFour pdfValue={fsedValue}/>
+                         </Document>
+                     </Previewer>
+                
+                {/* <div style={{height:"800px",width:"800px"}}>
+                    <Maps 
+                        center={{ lat: 51.505, lng: -0.09 }}
+                        zoom={17}
+                        scrollWheelZoom={false}
+                    >
+                        <Pin 
+                            position={{ lat: 51.505, lng: -0.09 }}
+                            PopupMessage={'Your are located here'}
+                            onChange={(e) => { console.log(e) }}
+                        />
+                    </Maps>
+                    
+                </div> */}
+                {/* <Box 
+                    width={'100%'}
+                     // para ma scroll sa gamay nga device
+                     maxH="100%"
+                     // para ma scroll sa gamay nga device
+                    maxWidth={500} 
+                    textAlign={'center'}
+                >
+                    <Center marginBottom={5}>
+                        <Image src={ecs_logo} width={'auto'} height={'40px'} textAlign={'center'} />
+                        <Text
+                            color={'gray.500'}
+                            fontWeight={'500'}
+                            paddingX={'15px'}
+                            display={{
+                                base: 'none',
+                                md: 'block',
+                                
+                            }}
+                        >
+                            Electronic Certification System
+                        </Text>
+                    </Center> */}
+                    
+                    
+                    { /* Login Card */ }
+                    {/* <Box 
+                        marginX={{
+                            base: 5,
+                            md: 0
+                        }}
+                    >
+                        <Box 
+                            bg={'white'} 
+                            width={'100%'}
+                            maxWidth={500} 
+                            
+                            border={'1px'}
+                            borderColor={'gray.200'}
+                            paddingX={{
+                                base: 5,
+                                md: 10
+
+                            }}
+                            paddingY={{
+                                base: 5,
+                                md: 10
+                            }}
+                        >
+                            <VStack spacing={5}>
+                                <Box width={'100%'}>
+                                    <FormControl isRequired>
+                                        <FormLabel htmlFor={'UserID'}>User ID</FormLabel>
+                                        <Input id={'UserID'} />
+                                    </FormControl>
+                                </Box>
+                                <Box width={'100%'}>
+                                    <FormControl isRequired>
+                                        <FormLabel htmlFor={'Password'}>Password</FormLabel>
+                                        <Input id={'Password'} type={'password'} />
+                                    </FormControl>
+                                </Box>
+                                <Box width={'100%'}>
+                                    <Button 
+                                        isLoading={loadingState}
+                                        spinner={<PulseLoader size={8} color='white' />}
+                                        colorScheme={'brand'} 
+                                        width={'100%'}
+                                        onClick={authFromBackend}
+                                    >
+                                        Login
+                                    </Button>
+                                </Box>
+                            </VStack>
+
+                            <Divider marginY={5} color={'gray.100'} />
+
+                            <Stack spacing={2} justifyContent={'center'}>
+                                <Box width={'100%'}>
+                                    <Text
+                                        fontSize={12}
+                                        color={'blackAlpha.600'}
+                                    >
+                                        Don't have an account?
+                                    </Text>
+                                    
+                                </Box>
+                                <Box>
+                                    <Button 
+                                        disabled={loadingState}
+                                        variant={'outline'}
+                                        size={'sm'}
+                                        paddingX={5}
+                                        paddingY={5}
+                                        onClick={(e) => navigate(PageBaseUrl.Auth.Register)}
+                                    >
+                                        Create an account
+                                    </Button>
+                                </Box>
+                            </Stack>
+                        </Box>
+                    </Box>  
+                    
+
+                    {/* Copyright & Application Information */}
+                    {/* <Box marginTop={5}>
+                        <Text 
+                            display={'block'}
+                            fontSize={14}
+                            color={'gray.400'}
+                            cursor={'pointer'}
+                            marginX={1}
+                        >
+                            © {new Date().getFullYear()} {process.env.REACT_APP_NAME}
+                        </Text>
+                        <Box 
+                            display={'block'}
+                            fontSize={14}
+                            color={'gray.400'}
+                            cursor={'pointer'}
+                            my={1}
+                            
+                        >
+                            *
+                            <Text display={'inline'}>Developer Information</Text>
+                            <Text display={'inline'}> - </Text>
+                           
+                            <Badge colorScheme={'green'} textTransform={'none'}>v{process.env.REACT_APP_VERSION}</Badge>
+                            
+                        </Box>
+                        
+                    </Box> */}
+                {/* </Box> */}
+            </Center>
+            
+            
+        </React.Fragment>
+        
+    );
+}
