8ebc96341da77565aa94bf1821cb81f1ca49f371
kyleowen12345 <gakyleowen@gmail.com>
2022-03-29 08:19:17 +0800

    chakra ui addition

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  MeetingProvider,
+  useMeeting,
+  useParticipant,
+  MeetingConsumer,
+} from "@videosdk.live/react-sdk";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
